c1b01e0cd24163df6a6acf847aeefed246a7d50c
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-10 14:19:02 -0800

    Enable functionality for palm

--- src/main.js
+++ src/main.js
@@ -10,22 +10,38 @@ let fern = plant.fernStateControl;
 let succulent = plant.succulentStateControl; 
 
 $(document).ready(function() {
+  // Iris Functionality
   $('#feed-iris').click(function() {
     const newState = iris(plant.feed);
     const currentState = iris();
     $('#iris-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
   });
-
   $('#water-iris').click(function() {
     const newState = iris(plant.water);
     const currentState = iris();
     $('#iris-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
   });
-
   $('#light-iris').click(function() {
     const newState = iris(plant.light);
     const currentState = iris();
     $('#iris-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
+  });
+
+   // Palm Functionality
+   $('#feed-palm').click(function() {
+    const newState = palm(plant.feed);
+    const currentState = palm();
+    $('#palm-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
+  });
+  $('#water-palm').click(function() {
+    const newState = palm(plant.water);
+    const currentState = palm();
+    $('#palm-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
+  });
+  $('#light-palm').click(function() {
+    const newState = palm(plant.light);
+    const currentState = palm();
+    $('#palm-stats').text(`Soil: ${currentState.soil} Water: ${currentState.water} Light: ${currentState.light}`);
   });
 
 
